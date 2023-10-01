ae09cd9b50ff61b1118d8ab61090078986ef7cc4
jheniffer <you@example.com>
2023-10-01 13:16:17 -0300

    terraform access aws

--- main.tf
+++ main.tf
@@ -10,16 +10,7 @@ terraform {
   required_version = ">= 1.2.0"
 }
 
-provider "aws" {
-  region = "us-west-2"
+resource "aws_s3_bucket" "github_backup" {
+  bucket = "jheni-tfstate-test"
+
 }
-
-resource "aws_instance" "app_server" {
-  ami           = "ami-08d70e59c07c61a3a"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = var.instance_name
-  }
-}
-
