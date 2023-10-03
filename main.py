23e849742f630cae4826104ba2d69c93f9079ba4
jheniffer <you@example.com>
2023-10-03 17:39:11 -0300

    changing if/else statment for try and except

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import requests
 from dotenv import dotenv_values
 
 
-def get_metadados(token):
+def get_metadata(token):
     my_headers = {'Authorization' : f'Bearer {token}'}
     response = requests.get('https://api.github.com/user/repos', headers=my_headers)
     resp = response.json()
@@ -17,7 +17,7 @@ def get_metadados(token):
 def download_repos(token, EXT='zip'):
     #EXT  = 'tar'  # it also works
     
-    metadados = get_metadados(token)
+    metadata = get_metadata(token)
     headers = {
         "Authorization" : f'token {token}',
         "Accept": 'application/vnd.github.v3+json'
@@ -25,20 +25,21 @@ def download_repos(token, EXT='zip'):
     
     REF  = ''      # master/main branch 
     
-    
-    
-    full_names = [repo['full_name'] for repo in metadados]
+    full_names = [repo['full_name'] for repo in metadata]
     for owner_repo  in full_names:
         repo_name = owner_repo.split('/')[-1]
         url = f'https://api.github.com/repos/{owner_repo}/{EXT}ball/{REF}'
-        r = requests.get(url, headers=headers)
-        if r.status_code == 200:
+        response = requests.get(url, headers=headers)
+        try:
+            response.raise_for_status()
             with open(f'repos/{repo_name}.{EXT}', 'wb') as fh:
-                fh.write(r.content)
-        else:
-            print(repo_name)
-            print(url)        
-            print(r.text)
+                fh.write(response.content)
+        except requests.exceptions.HTTPError as error_:
+            raise error_
+        except:
+            raise NotImplementedError(f"Can't catch this error: {response.status_code}")
+
+
 def main():
     secrets = dotenv_values(".env")
     access_token = secrets['github_token']
