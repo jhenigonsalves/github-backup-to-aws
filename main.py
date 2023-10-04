bd82d01cba93a7b04378550f6ef4d2c82e41e068
jheniffer <you@example.com>
2023-10-04 09:32:56 -0300

    adding typehints

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import requests
 from dotenv import dotenv_values
 import pathlib
+import os
+import typing
 
-def get_metadata(token, path_dir):
+
+def get_metadata(token: str, path_dir: typing.Union[str, bytes, os.PathLike]) -> list:
         
     my_headers = {'Authorization' : f'Bearer {token}'}
     response = requests.get('https://api.github.com/user/repos', headers=my_headers)
@@ -16,7 +19,7 @@ def get_metadata(token, path_dir):
             f.write(str(line)+'\n')
     return metadata 
 
-def download_repos(token, dir_name='repos/', EXT='zip'):
+def download_repos(token: str, dir_name: str='repos/', EXT: str='zip') -> None:
     #EXT  = 'tar'  # it also works
     path_dir = create_dir(dir_name)
     metadata = get_metadata(token, path_dir)
@@ -42,9 +45,10 @@ def download_repos(token, dir_name='repos/', EXT='zip'):
         except:
             raise NotImplementedError(f"Can't catch this error: {response.status_code}")
 
-def create_dir(dir_name):
+def create_dir(dir_name: str) -> str:
     path_dir = pathlib.Path(dir_name)
     path_dir.mkdir(parents=True, exist_ok=True)
+    print(type(path_dir))
     return path_dir
     
     
