e78763bd04fa3bdde91192256b3f5556a5c3749a
jheniffer <you@example.com>
2023-10-27 16:04:36 -0300

    adding test function to filter_repository_by_owner withh backup_only_owner_repos=True

--- tests/test_main.py
+++ tests/test_main.py
@@ -86,3 +86,19 @@ def test_filter_repository_by_owner_None(mock_owner_name):
     assert "octocat" in owners
     assert "github_user" in owners
     mock_owner_name.assert_not_called()
+
+
+@patch("main.get_owner_name")
+def test_filter_repository_by_owner_True(mock_owner_name):
+    mock_owner_name.return_value = "octocat"
+    repositories = [
+        {"name": "archive", "owner": "octocat", "is_private": True},
+        {"name": "file", "owner": "github_user", "is_private": True},
+    ]
+    repositories_returned = filter_repository_by_owner(repositories, "True")
+
+    owners = set([dict_["owner"] for dict_ in repositories_returned])
+    assert len(owners) == 1
+    assert "octocat" in owners
+    assert "github_user" not in owners
+    mock_owner_name.assert_called_once()
