4656ced73c1f990c13cbbdb1110c1f078325d6f5
jheniffer <you@example.com>
2023-10-27 12:43:00 -0300

    first draft test for get_owner_name

---
+++ tests/test_main.py
@@ -0,0 +1,56 @@
+from unittest.mock import patch, MagicMock
+from main import get_owner_name
+
+
+# Run these tests by invoking `$ python3 -m pytest tests`
+# https://docs.pytest.org/en/6.2.x/usage.html#:~:text=You%20can%20invoke%20testing%20through,the%20current%20directory%20to%20sys.
+@patch("requests.get")
+def test_get_owner_name(mock_response):
+    mock_response.response = {
+        "login": "octocat",
+        "id": 1,
+        "node_id": "MDQ6VXNlcjE=",
+        "avatar_url": "https://github.com/images/error/octocat_happy.gif",
+        "gravatar_id": "",
+        "url": "https://api.github.com/users/octocat",
+        "html_url": "https://github.com/octocat",
+        "followers_url": "https://api.github.com/users/octocat/followers",
+        "following_url": "https://api.github.com/users/octocat/following{/other_user}",
+        "gists_url": "https://api.github.com/users/octocat/gists{/gist_id}",
+        "starred_url": "https://api.github.com/users/octocat/starred{/owner}{/repo}",
+        "subscriptions_url": "https://api.github.com/users/octocat/subscriptions",
+        "organizations_url": "https://api.github.com/users/octocat/orgs",
+        "repos_url": "https://api.github.com/users/octocat/repos",
+        "events_url": "https://api.github.com/users/octocat/events{/privacy}",
+        "received_events_url": "https://api.github.com/users/octocat/received_events",
+        "type": "User",
+        "site_admin": "false",
+        "name": "monalisa octocat",
+        "company": "GitHub",
+        "blog": "https://github.com/blog",
+        "location": "San Francisco",
+        "email": "octocat@github.com",
+        "hireable": "false",
+        "bio": "There once was...",
+        "twitter_username": "monatheoctocat",
+        "public_repos": 2,
+        "public_gists": 1,
+        "followers": 20,
+        "following": 0,
+        "created_at": "2008-01-14T04:33:35Z",
+        "updated_at": "2008-01-14T04:33:35Z",
+        "private_gists": 81,
+        "total_private_repos": 100,
+        "owned_private_repos": 100,
+        "disk_usage": 10000,
+        "collaborators": 8,
+        "two_factor_authentication": "true",
+        "plan": {
+            "name": "Medium",
+            "space": 400,
+            "private_repos": 20,
+            "collaborators": 0,
+        },
+    }
+    owner_name = get_owner_name("")
+    assert owner_name == "octocat"
