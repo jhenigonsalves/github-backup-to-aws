61ca4136670e7eacb21a27235f2bf96b51a30052
jheniffer <you@example.com>
2023-10-27 15:36:15 -0300

    function test_create_dir add

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,9 +1,18 @@
 from unittest.mock import patch, MagicMock
+from main import create_dir
 from main import get_owner_name
+import pathlib
 
 
 # Run these tests by invoking `$ python3 -m pytest tests`
 # https://docs.pytest.org/en/6.2.x/usage.html#:~:text=You%20can%20invoke%20testing%20through,the%20current%20directory%20to%20sys.
+@patch("pathlib.Path.mkdir", autospec=True)
+def test_create_dir(pathlib_path):
+    pathlib_path.return_value = True
+    create_dir("tmpdir")
+    pathlib_path.assert_called_once()
+
+
 @patch("requests.get")
 def test_get_owner_name(mock_requests):
     mock_response = MagicMock()
