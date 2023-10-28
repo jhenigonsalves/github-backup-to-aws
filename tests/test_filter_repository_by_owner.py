4893f786017693e6ee513b7a04c10f499cd23fe3
jheniffer <you@example.com>
2023-10-28 13:26:48 -0300

    separint test functions in different files

---
+++ tests/test_filter_repository_by_owner.py
@@ -0,0 +1,39 @@
+from unittest.mock import patch, MagicMock
+
+from main import create_dir, get_owner_name, filter_repository_by_owner, get_url
+
+
+# Run these tests by invoking `$ python3 -m pytest tests`
+# https://docs.pytest.org/en/6.2.x/usage.html#:~:text=You%20can%20invoke%20testing%20through,the%20current%20directory%20to%20sys.
+
+
+@patch("main.get_owner_name")
+def test_filter_repository_by_owner_false(mock_owner_name):
+    mock_owner_name.return_value = "octocat"
+    repositories = [
+        {"name": "archive", "owner": "octocat", "is_private": True},
+        {"name": "file", "owner": "github_user", "is_private": True},
+    ]
+    repositories_returned = filter_repository_by_owner(repositories, "false")
+
+    owners = set([dict_["owner"] for dict_ in repositories_returned])
+    assert len(owners) > 1
+    assert "octocat" in owners
+    assert "github_user" in owners
+    mock_owner_name.assert_not_called()
+
+
+@patch("main.get_owner_name")
+def test_filter_repository_by_owner_true(mock_owner_name):
+    mock_owner_name.return_value = "octocat"
+    repositories = [
+        {"name": "archive", "owner": "octocat", "is_private": True},
+        {"name": "file", "owner": "github_user", "is_private": True},
+    ]
+    repositories_returned = filter_repository_by_owner(repositories, "True")
+
+    owners = set([dict_["owner"] for dict_ in repositories_returned])
+    assert len(owners) == 1
+    assert "octocat" in owners
+    assert "github_user" not in owners
+    mock_owner_name.assert_called_once()
---
+++ tests/test_get_url.py
@@ -0,0 +1,41 @@
+from unittest.mock import patch, MagicMock
+
+from main import create_dir, get_owner_name, filter_repository_by_owner, get_url
+
+
+# Run these tests by invoking `$ python3 -m pytest tests`
+# https://docs.pytest.org/en/6.2.x/usage.html#:~:text=You%20can%20invoke%20testing%20through,the%20current%20directory%20to%20sys.
+
+
+@patch("requests.get")
+def test_get_url(mock_requests_get):
+    mock_url = "foo"
+
+    mock_response = MagicMock()
+    mock_response.status_code = 200
+    mock_response.ok = True
+    mock_response.url = mock_url
+
+    mock_requests_get.return_value = mock_response
+    mocked_request = get_url(mock_url)
+    mock_requests_get.assert_called_once()
+    assert mocked_request.status_code == 200
+    assert mocked_request.url == "foo"
+
+
+@patch("requests.get")
+def test_get_url_with_params(mock_requests_get):
+    mock_url = "foo"
+    mock_headers = {"foo": "bar"}
+    mock_params = {"foo": "bar"}
+
+    mock_response = MagicMock()
+    mock_response.status_code = 200
+    mock_response.ok = True
+    mock_response.url = "foo"
+
+    mock_requests_get.return_value = mock_response
+    mocked_request = get_url(url=mock_url, headers=mock_headers, params=mock_params)
+    mock_requests_get.assert_called_once()
+    assert mocked_request.status_code == 200
+    assert mocked_request.url == "foo"
