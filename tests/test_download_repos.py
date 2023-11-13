f0243b8faf93ad4dc2e6446a308fd28f0b3e4758
jheniffer <you@example.com>
2023-11-13 18:26:28 -0300

    add test to download repos empty metadata

--- tests/test_download_repos.py
+++ tests/test_download_repos.py
@@ -8,6 +8,38 @@ from main import download_repos
 # https://docs.pytest.org/en/6.2.x/usage.html#:~:text=You%20can%20invoke%20testing%20through,the%20current%20directory%20to%20sys.
 
 
+# Run these tests by invoking `$ python3 -m pytest tests`
+# https://docs.pytest.org/en/6.2.x/usage.html#:~:text=You%20can%20invoke%20testing%20through,the%20current%20directory%20to%20sys.
+@patch("main.create_dir")
+@patch("main.get_metadata")
+@patch("main.get_url")
+@patch("main.write_repo_s3")
+def test_download_repos_empty_metadata(
+    mock_write_repo_s3,
+    mock_get_url,
+    mock_metadata,
+    mock_create_dir,
+):
+    mock_create_dir.return_value = None
+    mock_metadata.return_value = []
+    mock_get_url.return_value = None
+    mock_write_repo_s3.return_value = None
+
+    download_repos(
+        "foo_token",
+        "foo_bool",
+        "foo_prefix",
+        "foo_bucket",
+        "foo_dir_name",
+        "foo_EXT",
+    )
+
+    mock_create_dir.assert_called_once()
+    mock_metadata.assert_called_once()
+    mock_get_url.call_count == 0
+    mock_write_repo_s3.call_count == 0
+
+
 @patch("main.get_metadata")
 @patch("main.get_url")
 def test_download_repos_raise_http_error(patch_get_url, p_get_metadata):
