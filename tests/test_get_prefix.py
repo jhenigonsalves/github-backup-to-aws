c7826a29617758cd04c88224b43230ef53987e6b
Wesley Bortolozo <wesleyjr0101@gmail.com>
2023-11-14 14:30:31 -0300

    refactored tests for get_prefix using freezegun, KISS

--- tests/test_get_prefix.py
+++ tests/test_get_prefix.py
@@ -1,15 +1,13 @@
-from unittest.mock import patch
-
+from freezegun import freeze_time
 from main import get_prefix
 
 # Run these tests by invoking `$ python3 -m pytest tests`
 # https://docs.pytest.org/en/6.2.x/usage.html#:~:text=You%20can%20invoke%20testing%20through,the%20current%20directory%20to%20sys.
 
 
-@patch("main.get_current_date_formatted")
-def test_get_prefix(mock_formatted_date):
-    mock_formatted_date.return_value = "2012-12-10"
-    mocked_prefix = get_prefix("my_prefix")
+@freeze_time("2022-03-03")
+def test_get_prefix():
+    bucket_prefix = "my-prefix"
+    generated_prefix = get_prefix(bucket_prefix)
 
-    assert mocked_prefix == "my_prefix/2012-12-10"
-    mock_formatted_date.assert_called_once()
+    assert generated_prefix == f"{bucket_prefix}/2022-03-03"
