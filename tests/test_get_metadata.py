0ef9fb6d8848fc525472c7a830e43d46a9342c6e
jheniffer <you@example.com>
2023-11-14 11:29:20 -0300

    refactoring tests after the new function to write metadata in s3 bucket

--- tests/test_get_metadata.py
+++ tests/test_get_metadata.py
@@ -2,37 +2,46 @@ import pathlib
 from unittest.mock import patch, MagicMock
 
 from main import get_metadata
-
+import pytest
 
 # Run these tests by invoking `$ python3 -m pytest tests`
 # https://docs.pytest.org/en/6.2.x/usage.html#:~:text=You%20can%20invoke%20testing%20through,the%20current%20directory%20to%20sys.
 
 
-@patch("main.write_json")
 @patch("main.get_url")
 @patch("main.filter_repository_by_owner")
+@patch("json.dumps")
+@patch("main.write_metadata_in_s3_bucket")
 def test_is_get_metadata_not_executing_while_loop_if_response_empty(
-    mock_repositories, mock_requests_get, mock_write_json
+    mock_repositories,
+    mock_requests_get,
+    mock_json_dumps,
+    mock_write_metadata,
 ):
     mock_response = MagicMock()
     mock_response.json.return_value = []
     mock_requests_get.return_value = mock_response
-
+    mock_json_dumps.return_value = None
     mock_repositories.return_value = None
-    mock_write_json.return_value = None
+    mock_write_metadata.return_vaue = None
 
-    foo_path = pathlib.Path("foo_path")
-    get_metadata("foo_token", foo_path, "false")
+    get_metadata(
+        "foo_token",
+        "false",
+        "foo_prefix",
+        "foo_bucket_name",
+    )
 
     mock_requests_get.assert_called_once()
 
 
-@patch("main.write_json")
 @patch("main.get_url")
-def test_is_get_metadata_if_statment_true(
-    # mock_repositories,
+@patch("json.dumps")
+@patch("main.write_metadata_in_s3_bucket")
+def test_is_get_metadata_while_loop_executing(
+    mock_write_metadata,
+    mock_json_dumps,
     mock_requests_get,
-    mock_write_json,
 ):
     side_effect_1 = [
         {
@@ -60,19 +69,52 @@ def test_is_get_metadata_if_statment_true(
     mock3.json.return_value = side_effect_3
     mock_requests_get.side_effect = [mock1, mock2, mock3]
 
-    mock_write_json.return_value = None
+    mock_json_dumps.return_value = None
+    mock_write_metadata.return_vaue = None
 
-    foo_path = pathlib.Path("foo_path")
-    get_metadata("foo_token", foo_path, "false")
+    get_metadata(
+        "foo_token",
+        "false",
+        "foo_prefix",
+        "foo_bucket_name",
+    )
     assert mock_requests_get.call_count == 3
 
 
-@patch("main.write_json")
 @patch("main.get_url")
-def test_is_get_metadata_returning_a_dict(
-    # mock_repositories,
+@patch("main.filter_repository_by_owner")
+@patch("json.dumps")
+@patch("main.write_metadata_in_s3_bucket")
+def test_is_get_metadata_calling_filter_repository_by_owner(
+    mock_write_metadata,
+    mock_json_dumps,
+    mock_repositories,
     mock_requests_get,
-    mock_write_json,
+):
+    mock_response = MagicMock()
+    mock_response.json.return_value = []
+    mock_requests_get.return_value = mock_response
+    mock_json_dumps.return_value = None
+    mock_repositories.return_value = None
+    mock_write_metadata.return_vaue = None
+
+    get_metadata(
+        "foo_token",
+        "false",
+        "foo_prefix",
+        "foo_bucket_name",
+    )
+
+    mock_repositories.assert_called_once()
+
+
+@patch("main.get_url")
+@patch("json.dumps")
+@patch("main.write_metadata_in_s3_bucket")
+def test_is_get_metadata_returning_a_list_of_dicts(
+    mock_write_metadata_in_s3_bucket,
+    mock_json_dumps,
+    mock_requests_get,
 ):
     side_effect_1 = [
         {
@@ -82,22 +124,35 @@ def test_is_get_metadata_returning_a_dict(
             "private": "false",
         },
     ]
-    side_effect_2 = []
+    side_effect_2 = [
+        {
+            "id": 1296262349,
+            "name": "Hello",
+            "full_name": "octocat/Hello",
+            "private": "false",
+        },
+    ]
+    side_effect_3 = []
 
     mock1 = MagicMock()
     mock1.json.return_value = side_effect_1
     mock2 = MagicMock()
     mock2.json.return_value = side_effect_2
+    mock3 = MagicMock()
+    mock3.json.return_value = side_effect_3
+    mock_requests_get.side_effect = [mock1, mock2, mock3]
+    mock_json_dumps.return_value = None
+    mock_write_metadata_in_s3_bucket.return_value = None
 
-    mock_requests_get.side_effect = [mock1, mock2]
+    mocked_metadata = get_metadata(
+        "foo_token",
+        "false",
+        "foo_prefix",
+        "foo_bucket_name",
+    )
 
-    mock_write_json.return_value = None
-
-    foo_path = pathlib.Path("foo_path")
-    mocked_metadata = get_metadata("foo_token", foo_path, "false")
-
-    assert mock_requests_get.call_count == 2
-    assert len(mocked_metadata) == 1
+    assert mock_requests_get.call_count == 3
+    assert len(mocked_metadata) == 2
     assert type(mocked_metadata) == list
     assert type(mocked_metadata[0]) == dict
     assert "name" in mocked_metadata[0].keys()
