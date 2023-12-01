0e0a446aefc7af987617a0738b19b557649ab570
jheniffer <you@example.com>
2023-12-01 17:52:04 -0300

    creating variables file - terraform

---
+++ variables.tf
@@ -0,0 +1,26 @@
+variable "lambda_timeout"{
+    type = "number"
+    default = 10 # seconds
+
+}
+
+
+variable "memory_size"{
+    type = "number"
+    default = 1024 # MB
+
+}
+
+
+variable "package_type"{
+    type = "string"
+    default = "zip"
+
+}
+
+
+variable "runtime"{
+    type = "string"
+    default = "python3.11"
+
+}
