984703970da6e22e298e9014b68119014986c635
jheniffer <you@example.com>
2023-12-02 10:00:14 -0300

    creating cloudwatch log groups

--- data.tf
+++ data.tf
@@ -1,4 +1,4 @@
-data "archive_file" "lambda_encounter_type_general_zip" {
+data "archive_file" "lambda_github_backup_zip" {
   type        = "zip"
   source_file = "aux_files/lambda_function.py"
   output_path = local.lambda_github_backup.zip_file
--- lambda.tf
+++ lambda.tf
@@ -1,7 +1,7 @@
-resource "aws_lambda_layer_version" "github_backup_layer" {
+resource "aws_lambda_layer_version" "requests_ratelimit_layer" {
   s3_bucket  = "public-objects-31415"
   s3_key     = "lambda-layer/python.zip"
-  layer_name = "github_backup_layer"
+  layer_name = "requests_ratelimit_layer"
 
   compatible_runtimes = [var.runtime]
 }
@@ -9,17 +9,18 @@ resource "aws_lambda_layer_version" "github_backup_layer" {
 
 resource "aws_lambda_function" "github_backup" {
   filename         = local.lambda_github_backup.zip_file
-  function_name    = "github_backup"
+  function_name    = local.lambda_github_backup.name
   role             = aws_iam_role.github_backup_lambda_function_role.arn
   handler          = "lambda_function.lambda_handler"
   timeout          = var.lambda_timeout
   memory_size      = var.memory_size
   package_type     = var.package_type
-  source_code_hash = data.archive_file.lambda_encounter_type_general_zip.output_base64sha256
+  source_code_hash = data.archive_file.lambda_github_backup_zip.output_base64sha256
   runtime          = var.runtime
-  layers           = [aws_lambda_layer_version.github_backup_layer.arn]
+  layers           = [aws_lambda_layer_version.requests_ratelimit_layer.arn]
 
   depends_on = [
-    aws_iam_role.github_backup_lambda_function_role
+    aws_iam_role.github_backup_lambda_function_role,
+    aws_cloudwatch_log_group.lambda_function_cloudwatch_log_group
   ]
 }
